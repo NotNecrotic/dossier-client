8f4696de4fc210e4d66db07144b09e525ddc04c7
Necrotic <fearndevon@gmail.com>
2026-07-20 16:47:13 +1200

    Add back video controller

---
+++ Dossier.Client/Controllers/VideoController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dossier.Engine.Controllers;
+
+[ApiController]
+[Route("api/video")]
+public class VideoController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            return BadRequest("Path is required");
+
+        if (!System.IO.File.Exists(path))
+            return NotFound(path);
+
+        var extension =
+            Path.GetExtension(path)
+            .ToLowerInvariant();
+
+        var contentType = extension switch
+        {
+            ".mp4" => "video/mp4",
+            ".webm" => "video/webm",
+            ".ogg" => "video/ogg",
+            ".mov" => "video/quicktime",
+            ".mkv" => "video/x-matroska",
+            _ => "application/octet-stream"
+        };
+
+        return PhysicalFile(
+            path,
+            contentType,
+            enableRangeProcessing: true
+        );
+    }
+}
